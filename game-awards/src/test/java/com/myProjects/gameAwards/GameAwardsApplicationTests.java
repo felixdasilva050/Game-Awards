4614447469a841ef33050884b88e8f97a6ab5aaa
Guilherme <57736435+felixdasilva050@users.noreply.github.com>
2022-12-28 11:48:05 -0300

    initial commit

---
+++ game-awards/src/test/java/com/myProjects/gameAwards/GameAwardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myProjects.gameAwards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameAwardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
